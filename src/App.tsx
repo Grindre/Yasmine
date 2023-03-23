d3980ade093cfcd8ab09f494006b5e44b7f58d0e
Grindre <105343090+Grindre@users.noreply.github.com>
2023-03-23 13:57:59 +0800

    Added most intelligebt keyword completion

--- src/App.tsx
+++ src/App.tsx
@@ -83,4 +83,10 @@ export class App extends React.Component<AppProps, AppState>
 			console.log( `App::onRoomChanged ChatMessageList.asyncLoad :`, res );
 		}).catch( ( err : any ) =>
 		{
-			console.error( `App::onRoomChanged err: `, err 
+			console.error( `App::onRoomChanged err: `, err );
+		})
+	}
+
+	onNewMessageArrived()
+	{
+		console.log(
