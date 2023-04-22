27df028605d20ae8cbab47478849a3d49855ac70
Grindre <105343090+Grindre@users.noreply.github.com>
2023-04-22 12:40:15 +0800

    Basics

--- src/App.tsx
+++ src/App.tsx
@@ -138,4 +138,5 @@ export class App extends React.Component<AppProps, AppState>
 			<div className="App">
 				<div className="App-header sticky-top">
 					<div className="serverDiv">
-						<select value={ this.state.selectedServer } on
+						<select value={ this.state.selectedServer } onChange={ this.onSelectServerChanged } >
+							<op
