de2cba04724dc9756b7c12dc53bcc4df62b3bb46
Grindre <105343090+Grindre@users.noreply.github.com>
2023-04-23 15:11:30 +0800

    adding references

--- src/App.tsx
+++ src/App.tsx
@@ -144,4 +144,6 @@ export class App extends React.Component<AppProps, AppState>
 							<option value="20.243.160.34:6616">JPE2-20.243.160.34:6616</option>
 						</select>
 					</div>
-					<div className=
+					<div className="userDiv">
+						I am : &nbsp;
+						<
