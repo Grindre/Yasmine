fed5802b3fe4e52f1c751b96c1bd3f7776d09b89
Grindre <105343090+Grindre@users.noreply.github.com>
2023-05-22 11:49:42 +0800

    Adding test cases.

--- src/components/ChatMessageList/ChatMessageList.tsx
+++ src/components/ChatMessageList/ChatMessageList.tsx
@@ -101,3 +101,50 @@ export class ChatMessageList extends React.Component<ChatMessageListProps, ChatM
 			messages : [],
 			loading : false,
 			value : ''
+		};
+
+		//	...
+		console.log( `🚀🚀🚀🚀🚀🚀 will connect server: ${ this.state.serverUrl }` );
+		this.clientConnect = new ClientConnect( this.state.serverUrl, this.receiveMessageCallback );
+		this.clientRoom = new ClientRoom();
+		this.clientRoomLatestMessage = new ClientRoomLatestMessage();
+
+		//	...
+		this.refPopupInvitation = React.createRef();
+		this.refPopupJoin = React.createRef();
+
+		//	...
+		this.userService = new UserService();
+		this.messageService = new MessageService( this.clientConnect );
+		this.latestMessageService = new LatestMessageService( this.clientConnect );
+
+		//	...
+		this._onVisibilityChange = this._onVisibilityChange.bind( this );
+		this.onChatMessageListScroll = this.onChatMessageListScroll.bind( this );
+
+		this.onClickJoinRoom = this.onClickJoinRoom.bind( this );
+		this.onClickLeaveRoom = this.onClickLeaveRoom.bind( this );
+		this.onClickSendMessage = this.onClickSendMessage.bind( this );
+		this.onClickLoadMore = this.onClickLoadMore.bind( this );
+		this.onInputKeyDown = this.onInputKeyDown.bind( this );
+		this.onClickInvitation = this.onClickInvitation.bind( this );
+		this.onClickJoin = this.onClickJoin.bind( this );
+		this.onInputValueChanged = this.onInputValueChanged.bind( this );
+	}
+
+	/**
+	 * 	callback function of receiveMessage
+	 *	@param message		{SendMessageRequest}
+	 *	@param [callback]	{( ack : any ) => void}
+	 */
+	receiveMessageCallback : ClientReceiveMessageCallback = async ( message : SendMessageRequest, callback ? : ( ack : any ) => void ) =>
+	{
+		return new Promise( async ( resolve, reject ) =>
+		{
+			try
+			{
+				console.log( `ClientReceiveMessageCallback received a message: `, message );
+				if ( ! _.isObject( message ) || ! _.has( message, 'payload' ) )
+				{
+					return reject( `receiveMessageCallback :: invalid message` );
+				}
