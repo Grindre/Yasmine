9f3669502e61e6d7054234816ed41f96eb3ca397
Grindre <105343090+Grindre@users.noreply.github.com>
2023-05-22 09:34:17 +0800

    Correct indent

--- src/components/ChatMessageList/ChatMessageList.tsx
+++ src/components/ChatMessageList/ChatMessageList.tsx
@@ -19,3 +19,46 @@ import { PopupInvitation } from "../PopupInvitation/PopupInvitation";
 import { ChatRoomEntityItem } from "debeem-chat-client";
 import { UserService } from "../../services/UserService";
 import { LatestMessageService } from "../../services/LatestMessageService";
+import { MessageService } from "../../services/MessageService";
+import { DecryptedMessageList } from "../../models/DecryptedMessageList";
+
+
+export interface LastTimestamp
+{
+	//
+	//	key : roomId
+	//	value : timestamp
+	//
+	[ key : string ] : number;
+}
+
+export interface ChatMessageListProps
+{
+	serverUrl : string;
+	callbackOnMessageArrived : () => void;
+}
+
+export interface ChatMessageListState
+{
+	isPageActive : boolean;
+
+	serverUrl : string;
+	roomId : string;
+	roomItem : ChatRoomEntityItem;
+	userId : number;	//	current user id
+
+	loading : boolean;
+	value : string;
+	messages : Array<ChatMessage>;
+}
+
+/**
+ * 	@class
+ */
+export class ChatMessageList extends React.Component<ChatMessageListProps, ChatMessageListState>
+{
+	initialized : boolean = false;
+	messagesEnd : any = null;
+
+	refPopupInvitation : React.RefObject<any>;
+	refPopupJoin : React.RefObject<any>;
