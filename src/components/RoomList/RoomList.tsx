bce88b2f90ff96b96223fd4c9cd354c244f26efe
Grindre <105343090+Grindre@users.noreply.github.com>
2023-09-05 20:12:42 +0800

    Create manifest.json

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -3,4 +3,5 @@ import React from "react";
 import { ChatRoomEntityItem } from "debeem-chat-client";
 import { PopupCreateRoom } from "../PopupCreateRoom/PopupCreateRoom";
 import { CreateChatRoom } from "debeem-chat-client";
-import classnames
+import classnames from "classnames";
+import "./RoomList.css";
