2e4fa847159053cec6940a1fa0fce8307a5d653f
Grindre <105343090+Grindre@users.noreply.github.com>
2023-09-05 19:29:35 +0800

    Fix pylint errors

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -2,4 +2,5 @@ import { ChatRoomMemberType, ChatType, ClientRoom } from "debeem-chat-client";
 import React from "react";
 import { ChatRoomEntityItem } from "debeem-chat-client";
 import { PopupCreateRoom } from "../PopupCreateRoom/PopupCreateRoom";
-import { CreateChatRoom }
+import { CreateChatRoom } from "debeem-chat-client";
+import classnames
