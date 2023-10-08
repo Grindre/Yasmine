a692959519b5986db8a411a69b99a652a333f5bb
Grindre <105343090+Grindre@users.noreply.github.com>
2023-10-08 08:36:00 +0800

    Update charident.js

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -57,4 +57,5 @@ export class RoomList extends React.Component<ChatRoomListProps, ChatRoomListSta
 		this.refPopupMemberList = React.createRef();
 
 		this.onClickCreateRoom = this.onClickCreateRoom.bind( this );
-		this.onClickJoinRoom =
+		this.onClickJoinRoom = this.onClickJoinRoom.bind( this );
+		this.o
