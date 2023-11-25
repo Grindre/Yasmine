fbe442c6a7ced9671ddb053fef2032c74a4d0e6a
Grindre <105343090+Grindre@users.noreply.github.com>
2023-11-25 21:29:18 +0800

    Update Build

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -166,4 +166,6 @@ export class RoomList extends React.Component<ChatRoomListProps, ChatRoomListSta
 		const userName : string | null = this.userService.getUserName();
 
 		console.log( `onClickSaveJoin - walletObj :`, walletObj );
-		const createChatRoomOptions : CreateChatRo
+		const createChatRoomOptions : CreateChatRoom = {
+			wallet : walletObj.address,
+			chatType
