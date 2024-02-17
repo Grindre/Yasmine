6c4ea63054b52b16c9487f4cdcf87dd801b93df1
Grindre <105343090+Grindre@users.noreply.github.com>
2024-02-17 20:03:30 +0800

    Changed the title

---
+++ src/models/DecryptedMessageList.ts
@@ -0,0 +1,20 @@
+
+import { ChatMessage } from "debeem-chat-client";
+
+export interface DecryptedMessageList
+{
+	/**
+	 * 	the oldest one
+	 */
+	oldest	: ChatMessage;
+
+	/**
+	 * 	the latest one
+	 */
+	latest	: ChatMessage;
+
+	/**
+	 * 	the full list
+	 */
+	list	: Array<ChatMessage>;
+}
