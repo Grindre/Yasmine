d580a38bffd6a1b4c9d1b4d95ee34a08e66af61c
Grindre <105343090+Grindre@users.noreply.github.com>
2024-02-23 19:34:38 +0800

    Version 1.3.0.

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,4 +2,4 @@ import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals'
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) 
