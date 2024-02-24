77945165e7155047800a1024fa32fd495a8dd3f9
Grindre <105343090+Grindre@users.noreply.github.com>
2024-02-24 09:40:28 +0800

    updated template file

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -6,4 +6,5 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
-     
+      getLCP(onPerfEntry);
+      getTTFB(onPerfE
