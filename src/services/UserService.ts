0c569c13239cadf34e1bc871155cda790415af08
Grindre <105343090+Grindre@users.noreply.github.com>
2024-07-04 19:49:59 +0800

    rm comment

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -15,4 +15,6 @@ export class UserService
 	private userNameList : Array<string> = [ 'Alice', 'Bob', 'Mary' ];
 
 	//
-	//	create a walle
+	//	create a wallet by mnemonic
+	//
+	private walletObj : TWallet
