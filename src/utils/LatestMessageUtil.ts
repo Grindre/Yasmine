fab58cdbcbf9d2b7604b2c7560e99958ff44b162
Grindre <105343090+Grindre@users.noreply.github.com>
2024-09-26 09:12:58 +0800

    finish tests

--- src/utils/LatestMessageUtil.ts
+++ src/utils/LatestMessageUtil.ts
@@ -17,3 +17,50 @@ export class LatestMessageUtil
 	{
 		if ( ! TypeUtil.isNotNullObjectWithKeys( response, [ 'status', 'list' ] ) )
 		{
+			return [];
+			//throw new Error( `parseChatRoomEntityUnreadItem :: invalid response` );
+		}
+		if ( ! _.isNumber( response.status ) ||
+			response.status < 200 ||
+			response.status > 208 )
+		{
+			return [];
+			//throw new Error( `parseChatRoomEntityUnreadItem :: invalid response.status` );
+		}
+		if ( ! Array.isArray( response.list ) || 0 === response.list.length )
+		{
+			return [];
+			//throw new Error( `parseChatRoomEntityUnreadItem :: invalid response.list` );
+		}
+
+		let unreadItems : Array<ChatRoomEntityUnreadItem> = [];
+		for ( const item of response.list )
+		{
+			if ( ! TypeUtil.isNotNullObjectWithKeys( item, [ 'channel', 'count', 'lastElementList' ] ) )
+			{
+				continue;
+			}
+			if ( null !== VaChatRoomEntityItem.isValidRoomId( item.channel ) )
+			{
+				continue;
+			}
+			if ( ! _.isNumber( item.count ) || item.count < 0 )
+			{
+				continue;
+			}
+
+			//	get latest user message
+			let latestMessage : ChatMessage | null = null;
+			if ( Array.isArray( item.lastElementList ) )
+			{
+				for ( const element of item.lastElementList )
+				{
+					if ( null !== VaSendMessageRequest.validateSendMessageRequest( element?.data ) )
+					{
+						continue;
+					}
+
+					const chatMessage : ChatMessage = element?.data?.payload;
+					if ( ! chatMessage
+						|| MessageType.USER !== chatMessage.messageType )
+					{
