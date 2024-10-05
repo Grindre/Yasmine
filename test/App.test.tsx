4fef88e4ee992db7afed4fe9f49b5d88ef027615
Grindre <105343090+Grindre@users.noreply.github.com>
2024-10-05 09:46:53 +0800

    New translations p00_ch02_intro.md (Indonesian)

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -5,4 +5,5 @@ import App from '../src/App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeI
+  expect(linkElement).toBeInTheDocument();
+});
